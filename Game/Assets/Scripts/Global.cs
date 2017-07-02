6e697066cdfa2b82ee49895825234287bf0806f4
BuildTools <unconfigured@null.spigotmc.org>
2017-07-02 22:32:21 +0200

    added global configs

---
+++ Game/Assets/Scripts/Global.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Network;
+using FileManager;
+
+public static class Global {
+
+	public static int Port = 5092;
+	public static string Host = "127.0.0.1";
+	public static string ConfigurationFilePath = "config.ini";
+
+	public static UdpClient Client { get; private set; }
+
+	public static Config ConfigurationFile { get; private set; }
+
+	[RuntimeInitializeOnLoadMethod]
+	public static void Init(){
+
+		ConfigurationFile  = new Config(ConfigurationFilePath); 
+
+		Client = new UdpClient (Host, Port);
+		Client.Connect ();
+	}
+}
