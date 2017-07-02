a6449be47676920d72341291b9f83728d6331070
BuildTools <unconfigured@null.spigotmc.org>
2017-07-02 22:31:40 +0200

    auto generated

---
+++ Game/Assets/Resources/OptionsScreenObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsScreenObject : MonoBehaviour {
+
+	private Button _buttonBack;
+
+	// Use this for initialization
+	void Start () {
+		_buttonBack = GetComponentInChildren<Button> ();
+		_buttonBack.onClick.AddListener (delegate {
+			GameObject.Find ("MenuContainer").GetComponent<Animator>().SetTrigger("LoginScreen");
+		});
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
