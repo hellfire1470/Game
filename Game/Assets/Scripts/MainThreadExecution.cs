bebce9c721c834274dd92b4fbdfa3d9c5be9ba88
BuildTools <unconfigured@null.spigotmc.org>
2017-07-02 22:32:53 +0200

    added OnMainThread Object

---
+++ Game/Assets/Scripts/MainThreadExecution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainThreadExecution : MonoBehaviour {
+
+
+
+	private static List<Action> _actions = new List<Action>();
+
+	public static void OnMainThread(Action action){
+		lock (_actions) {
+			_actions.Add (action);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lock (_actions) {
+			foreach (Action action in _actions) {
+				action ();
+			}
+			_actions.Clear ();
+		}
+	}
+}
