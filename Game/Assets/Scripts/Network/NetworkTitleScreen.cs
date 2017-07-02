d314436b01379f484d8f5452ac0d77583cf78ea9
BuildTools <unconfigured@null.spigotmc.org>
2017-07-02 22:33:16 +0200

    added NetworkScript for TitleScreen

---
+++ Game/Assets/Scripts/Network/NetworkTitleScreen.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Network;
+using Network.Packages;
+
+public class NetworkTitleScreen: MonoBehaviour {
+
+	void Start(){
+		Global.Client.DataReceived += DataReceived;
+	}
+
+	void Disable(){
+		Global.Client.DataReceived -= DataReceived;
+	}
+
+	public void DataReceived(object sender, NetworkReceiveEventArgs e){
+		Response response;
+		switch (NetworkHelper.GetPackageType (e.Data)) {
+		case PackageType.Login:
+			response = NetworkHelper.Deserialize<LoginResponse> (e.Data);
+			if (response.Success) {
+				MainThreadExecution.OnMainThread (delegate {
+					GameObject.Find ("MenuContainer").GetComponent<Animator> ().SetTrigger ("CharacterSelection");
+				});
+			} else {
+				MainThreadExecution.OnMainThread (delegate {
+					GameObject.Find ("LoginScreen").GetComponent<Animator> ().SetTrigger ("LoginFail");
+				});
+			}
+			break;
+		}
+	}
+}
